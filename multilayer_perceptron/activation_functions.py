11afb4640076cc59b01aa1e9392081250f2c8241
tsannie <tsannie@student.42.fr>
2023-04-26 04:41:59 +0200

    [+] basic functions

--- multilayer_perceptron/activation_functions.py
+++ multilayer_perceptron/activation_functions.py
@@ -15,3 +15,25 @@ def softmax(x, axis=-1):
     """Softmax activation function. range [0, 1]"""
     reduce_sum = np.sum(np.exp(x), axis=axis, keepdims=True)
     return np.exp(x) / reduce_sum
+
+
+def softplus(x):
+    """Softplus activation function. range [0, inf)"""
+    return np.log(1 + np.exp(x))
+
+
+def softsign(x):
+    """Softsign activation function. range [-1, 1]"""
+    return x / (1 + np.abs(x))
+
+
+def tanh(x):
+    """Tanh activation function. range [-1, 1]"""
+    sinh = np.exp(x) - np.exp(-x)
+    cosh = np.exp(x) + np.exp(-x)
+    return sinh / cosh
+
+
+def exponential(x):
+    """Exponential activation function. range [0, inf)"""
+    return np.exp(x)
--- multilayer_perceptron/tests/test_activation_functions.py
+++ multilayer_perceptron/tests/test_activation_functions.py
@@ -50,7 +50,46 @@ class TestActivations(unittest.TestCase):
 
             keras_output = keras_softmax(tensor)
             my_output = my_softmax(scores)
-            print(keras_output, " == ", my_output)
+            np.testing.assert_array_almost_equal(keras_output, my_output)
+
+    def test_softplus(self):
+        keras_softplus = keras_activations.softplus
+        my_softplus = my_activations.softplus
+
+        for x in self.inputs:
+            keras_output = keras_softplus(x)
+            my_output = my_softplus(x)
+
+            np.testing.assert_array_almost_equal(keras_output, my_output)
+
+    def test_softsign(self):
+        keras_softsign = keras_activations.softsign
+        my_softsign = my_activations.softsign
+
+        for x in self.inputs:
+            keras_output = keras_softsign(x)
+            my_output = my_softsign(x)
+
+            np.testing.assert_array_almost_equal(keras_output, my_output)
+
+    def test_tanh(self):
+        keras_tanh = keras_activations.tanh
+        my_tanh = my_activations.tanh
+
+        for x in self.inputs:
+            keras_output = keras_tanh(x)
+            my_output = my_tanh(x)
+
+            np.testing.assert_array_almost_equal(keras_output, my_output)
+
+    def test_exponential(self):
+        keras_exponential = keras_activations.exponential
+        my_exponential = my_activations.exponential
+
+        for x in self.inputs:
+            keras_output = keras_exponential(x)
+            my_output = my_exponential(x)
+
             np.testing.assert_array_almost_equal(keras_output, my_output)
 
 
