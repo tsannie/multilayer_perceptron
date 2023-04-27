[fix] derivative functions

multilayer_perceptron/activations.py
@@ -1,50 +1,99 @@
 import numpy as np
 
 
-def relu(x):
-    """ReLU activation function. range [0, inf)"""
-    return np.maximum(x, 0)
+class Activation:
+    def __call__(self, x, derivative=False):
+        if derivative:
+            return self.derivative(x)
+        else:
+            return self.activate(x)
+
+    def activate(self, x):
+        raise NotImplementedError
+
+    def derivative(self, x):
+        raise NotImplementedError
 
 
-def sigmoid(x):
-    """Sigmoid activation function. range [0, 1]"""
-    return 1 / (1 + np.exp(-x))
+class ReLU(Activation):
+    """ReLU activation activate. range [0, inf)"""
+
+    def activate(self, x):
+        return np.maximum(x, 0)
+
+    def derivative(self, x):
+        return np.where(x > 0, 1, 0)
 
 
-def softmax(x, axis=-1):
-    """Softmax activation function. range [0, 1]"""
-    reduce_sum = np.sum(np.exp(x), axis=axis, keepdims=True)
-    return np.exp(x) / reduce_sum
+class Sigmoid(Activation):
+    """Sigmoid activation activate. range [0, 1]"""
+
+    def activate(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x):
+        return self.activate(x) * (1 - self.activate(x))
 
 
-def softplus(x):
-    """Softplus activation function. range [0, inf)"""
-    return np.log(1 + np.exp(x))
+class Softmax(Activation):
+    """Softmax activation activate. range [0, 1]"""
+
+    def activate(self, x):
+        reduce_sum = np.sum(np.exp(x), axis=-1, keepdims=True)
+        return np.exp(x) / reduce_sum
+
+    def derivative(self, x):
+        return self.activate(x) * (1 - self.activate(x))
 
 
-def softsign(x):
-    """Softsign activation function. range [-1, 1]"""
-    return x / (1 + np.abs(x))
+class Softplus(Activation):
+    """Softplus activation activate. range [0, inf)"""
+
+    def activate(self, x):
+        return np.log(1 + np.exp(x))
+
+    def derivative(self, x):
+        return 1 / (1 + np.exp(-x))
 
 
-def tanh(x):
-    """Tanh activation function. range [-1, 1]"""
-    sinh = np.exp(x) - np.exp(-x)
-    cosh = np.exp(x) + np.exp(-x)
-    return sinh / cosh
+class Softsign(Activation):
+    """Softsign activation activate. range [-1, 1]"""
+
+    def activate(self, x):
+        return x / (1 + np.abs(x))
+
+    def derivative(self, x):
+        return 1 / (1 + np.abs(x)) ** 2
 
 
-def exponential(x):
-    """Exponential activation function. range [0, inf)"""
-    return np.exp(x)
+class Tanh(Activation):
+    """Tanh activation activate. range [-1, 1]"""
+
+    def activate(self, x):
+        sinh = np.exp(x) - np.exp(-x)
+        cosh = np.exp(x) + np.exp(-x)
+        return sinh / cosh
+
+    def derivative(self, x):
+        return 1 - self.activate(x) ** 2
+
+
+class Exponential(Activation):
+    """Exponential activation activate. range [0, inf)"""
+
+    def activate(self, x):
+        return np.exp(x)
+
+    def derivative(self, x):
+        return self.activate(x)
 
 
 ACTIVATION_FUNCTIONS = {
-    "relu": relu,
-    "sigmoid": sigmoid,
-    "softmax": softmax,
-    "softplus": softplus,
-    "softsign": softsign,
-    "tanh": tanh,
-    "exponential": exponential,
+    "relu": ReLU,
+    "sigmoid": Sigmoid,
+    "softmax": Softmax,
+    "softplus": Softplus,
+    "softsign": Softsign,
+    "tanh": Tanh,
+    "exponential": Exponential,
 }

multilayer_perceptron/dense_layer.py
@@ -1,7 +1,6 @@
 import numpy as np
-from initializers import *
-from activations import ACTIVATION_FUNCTIONS
-from initializers import WEIGHTS_INITIALIZERS
+import initializers
+import activations
 
 
 def check_arguments(valid_options):
@@ -29,16 +28,19 @@ class DenseLayer:
         self.n_neurons = n_neurons
         self.weights = None
         self.bias = None
+        self.z = None
+        self.X = None
+        self.A = None
 
-        @check_arguments(ACTIVATION_FUNCTIONS)
+        @check_arguments(activations.ACTIVATION_FUNCTIONS)
         def set_activation(self, argument):
             self.activation = argument
 
-        @check_arguments(WEIGHTS_INITIALIZERS)
+        @check_arguments(initializers.WEIGHTS_INITIALIZERS)
         def set_weights_initializer(self, argument):
             self.weights_initializer = argument
 
-        @check_arguments(WEIGHTS_INITIALIZERS)
+        @check_arguments(initializers.WEIGHTS_INITIALIZERS)
         def set_bias_initializer(self, argument):
             self.bias_initializer = argument
 
@@ -49,3 +51,15 @@ class DenseLayer:
     def initialize(self, shape):
         self.weights = self.weights_initializer(shape)
         self.bias = self.bias_initializer((1, self.n_neurons))
+
+    def forward(self, X):
+        self.X = X
+        self.z = np.dot(self.X, self.weights) + self.bias
+        self.A = self.activation()(self.z)
+        return self.A
+
+    def backward(self, dA):
+        m = self.X.shape[0]
+
+        # calcul derivate of activation function
+        # dZ = dA * self.activation(self.z, derivative=True)

multilayer_perceptron/tests/test_dense_layer.py
@@ -22,7 +22,7 @@ class TestDenseLayer(unittest.TestCase):
         )
 
     def test_init(self):
-        self.assertEqual(self.dense_layer.activation, activations.relu)
+        self.assertEqual(self.dense_layer.activation, activations.ReLU)
         self.assertEqual(self.dense_layer.weights_initializer, self.weights_initializer)
         self.assertEqual(self.dense_layer.bias_initializer, self.bias_initializer)
         self.assertEqual(self.dense_layer.n_neurons, self.n_neurons)
@@ -36,15 +36,15 @@ class TestDenseLayer(unittest.TestCase):
             weights_initializer=self.weights_initializer,
             bias_initializer=self.bias_initializer,
         )
-        self.assertEqual(self.dense_layer.activation, activations.relu)
+        self.assertEqual(self.dense_layer.activation, activations.ReLU)
 
         self.dense_layer = DenseLayer(
             n_neurons=self.n_neurons,
-            activation=activations.relu,
+            activation=activations.ReLU(),
             weights_initializer=self.weights_initializer,
             bias_initializer=self.bias_initializer,
         )
-        self.assertEqual(self.dense_layer.activation, activations.relu)
+        self.assertTrue(isinstance(self.dense_layer.activation, activations.ReLU))
 
     def test_init_with_str_or_callable_weights_initializer(self):
         self.dense_layer = DenseLayer(
@@ -53,17 +53,16 @@ class TestDenseLayer(unittest.TestCase):
             weights_initializer="glorot_normal",
             bias_initializer="zeros",
         )
-        self.assertTrue(
-            isinstance(self.dense_layer.weights_initializer, initializers.GlorotNormal)
+        self.assertEqual(
+            self.dense_layer.weights_initializer, initializers.GlorotNormal
         )
-        self.assertTrue(
-            isinstance(self.dense_layer.bias_initializer, initializers.Zeros)
-        )
-        self.assertEqual(self.dense_layer.activation, activations.relu)
+        self.assertEqual(self.dense_layer.bias_initializer, initializers.Zeros)
+
+        self.assertEqual(self.dense_layer.activation, activations.ReLU)
 
         self.dense_layer = DenseLayer(
             n_neurons=self.n_neurons,
-            activation=activations.relu,
+            activation=activations.ReLU(),
             weights_initializer=initializers.GlorotNormal(),
             bias_initializer=initializers.Zeros(),
         )
@@ -73,7 +72,7 @@ class TestDenseLayer(unittest.TestCase):
         self.assertTrue(
             isinstance(self.dense_layer.bias_initializer, initializers.Zeros)
         )
-        self.assertEqual(self.dense_layer.activation, activations.relu)
+        self.assertTrue(isinstance(self.dense_layer.activation, activations.ReLU))
 
     def test_init_with_invalid_arg(self):
         try:
@@ -99,7 +98,7 @@ class TestDenseLayer(unittest.TestCase):
             pass
 
     def test_initialize(self):
-        shape = (5, 10)
+        shape = (5, self.n_neurons)
         self.dense_layer.initialize(shape)
 
         self.assertEqual(self.dense_layer.weights.shape, shape)
@@ -108,6 +107,16 @@ class TestDenseLayer(unittest.TestCase):
         self.assertTrue(np.all(self.dense_layer.weights >= -1))
         self.assertTrue(np.all(self.dense_layer.weights <= 1))
         self.assertTrue(np.all(self.dense_layer.bias == 0))
+
+    def test_forward(self):
+        shape = (5, self.n_neurons)
+        m = 50
+        self.dense_layer.initialize(shape)
+
+        inputs = np.random.randn(m, shape[0])
+        outputs = self.dense_layer.forward(inputs)
+
+        self.assertEqual(outputs.shape, (50, self.n_neurons))
 
 
 if __name__ == "__main__":