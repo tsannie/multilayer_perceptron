7a445d24a440bd70c884ec9a29ed995bc9a66d44
tsannie <tsannie@student.42.fr>
2023-05-25 19:29:02 +0200

    [+] metrics

--- multilayer_perceptron/metrics.py
+++ multilayer_perceptron/metrics.py
@@ -12,6 +12,46 @@ class Metric:
         raise NotImplementedError
 
 
+class Precision(Metric):
+    def __init__(self):
+        super().__init__("precision")
+        self.true_positives = 0
+        self.false_positives = 0
+
+    def update_state(self, y_true, y_pred):
+        y_true = np.round(y_true)
+        y_pred = np.round(y_pred)
+        self.true_positives += np.sum(y_true * y_pred)
+        self.false_positives += np.sum((1 - y_true) * y_pred)
+
+    def reset_state(self):
+        self.true_positives = 0
+        self.false_positives = 0
+
+    def result(self):
+        return self.true_positives / (self.true_positives + self.false_positives)
+
+
+class Recall(Metric):
+    def __init__(self):
+        super().__init__("recall")
+        self.true_positives = 0
+        self.false_negatives = 0
+
+    def update_state(self, y_true, y_pred):
+        y_true = np.round(y_true)
+        y_pred = np.round(y_pred)
+        self.true_positives += np.sum(y_true * y_pred)
+        self.false_negatives += np.sum(y_true * (1 - y_pred))
+
+    def reset_state(self):
+        self.true_positives = 0
+        self.false_negatives = 0
+
+    def result(self):
+        return self.true_positives / (self.true_positives + self.false_negatives)
+
+
 class MeanMetric(Metric):
     def __init__(self, name):
         super().__init__(name)
